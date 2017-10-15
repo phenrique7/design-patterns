ed5e5fccc77bf74465abd67a72fe31f084d99715
phenrique7 <pauloh1288@gmail.com>
2017-10-15 17:34:33 -0200

    Modificação na classe AdapterPattern

--- src/AdapterPattern/AdapterPattern.java
+++ src/AdapterPattern/AdapterPattern.java
@@ -1,4 +1,10 @@
 package AdapterPattern;
 
 public class AdapterPattern {
+
+    public static void main(String[] args){
+        Cliente cliente = new Cliente(new SomadorAdapter());
+        cliente.executar();
+    }
+
 }
